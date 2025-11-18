bbfe77a4f3191368692dee31de74f51430fb124e
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-11-18 11:01:05 +0700

    add structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,18 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { StyleSheet, View } from 'react-native';
+import HomeScreen from '.';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <View style={styles.appContainer}>
+    <HomeScreen/>
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  appContainer:{
+    flex:1,
+    padding:8,
+    backgroundColor:"#E44631"
+  }
+})
