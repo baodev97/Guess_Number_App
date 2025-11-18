5f3cdef70ea45bb3b0819bff0e83987b9d5000b0
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-11-18 10:57:57 +0700

    creatting custom button

---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+type PrimaryButtonProps = {
+    children: ReactNode
+}
+export default function PrimaryButton({children}:PrimaryButtonProps) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- screen/StartGameScreen.tsx
+++ screen/StartGameScreen.tsx
@@ -1,11 +1,33 @@
-import { StyleSheet } from "react-native";
+import PrimaryButton from "@/components/PrimaryButton";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function StartGameScreen(){
     return (
-        <></>
+        <View>
+            <TextInput></TextInput>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
-})
+  inputContainer: {
+    marginTop: 100,
+    marginVertical: 20,
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: "#8C142E",
+  },
+  textInputContainer: {
+    borderBottomColor: "#E4DB31",
+    borderBottomWidth: 4,
+    color: "#E4DB31",
+  },
+  buttonContainer:{
+    flexDirection:"row"
+  },
+  textContainer: {
+    color: "black",
+  },
+});
