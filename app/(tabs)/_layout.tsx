90f2a326d262a5572df0121406df7df6fb8fca20
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-11-18 22:00:19 +0700

    adding a background image

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,27 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import HomeScreen from '.';
-
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { ImageBackground, StyleSheet } from "react-native";
+import HomeScreen from ".";
 
 export default function TabLayout() {
   return (
-    <LinearGradient colors={['#E44631','#ddb52f']} style={styles.appContainer}>
-    <HomeScreen/>
+    <LinearGradient colors={["#E44631", "#ddb52f"]} style={styles.appContainer}>
+      <ImageBackground
+        style={styles.appContainer}
+        source={require("../../assets/images/riho-kroll-m4sGYaHYN5o-unsplash.jpg")}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+      >
+        <HomeScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 const styles = StyleSheet.create({
-  appContainer:{
-    flex:1,
-    padding:8,
-    // backgroundColor:"#E44631"
+  appContainer: {
+    flex: 1,
+  },
+  backgroundImage:{
+    opacity:0.25
   }
-})
+});
