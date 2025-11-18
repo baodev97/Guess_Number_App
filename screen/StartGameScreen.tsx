b0cb8d74e828441ea2e829fae1560f08b07346b5
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-11-18 21:30:11 +0700

    styling Input container

--- screen/StartGameScreen.tsx
+++ screen/StartGameScreen.tsx
@@ -3,25 +3,33 @@ import { StyleSheet, TextInput, View } from "react-native";
 
 export default function StartGameScreen() {
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.numberInput}
-        maxLength={2}
-        keyboardType="number-pad"
-        autoCorrect={false}
-        autoCapitalize="none"
-      ></TextInput>
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.inputOuterContainer}>
+      <View style={styles.inputInnerContainer}>
+        <TextInput
+          style={styles.numberInput}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCorrect={false}
+          autoCapitalize="none"
+        ></TextInput>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonInnerContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View >
+        <View style={styles.buttonInnerContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  inputContainer: {
+  inputOuterContainer: {
     marginTop: "25%",
     marginHorizontal: 24,
-    padding: 16,
+    padding: 24,
     borderRadius: 8,
     backgroundColor: "#5e0431ff",
     elevation: 48,
@@ -30,8 +38,12 @@ const styles = StyleSheet.create({
     shadowRadius: 6,
     shadowOpacity: 0.25,
   },
+  inputInnerContainer: {
+    alignItems: "center",
+    marginBottom: 8,
+  },
   numberInput: {
-    height: 50,
+    height: 70,
     width: 50,
     fontSize: 32,
     borderBottomColor: "#ddb52f",
@@ -40,4 +52,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonInnerContainer:{
+    width:120
+  }
 });
