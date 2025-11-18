b3e680b3aed4cd25cd9074a11a8eff8d99b3b8bf
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-11-18 10:23:55 +0700

    setting up our screen components

---
+++ screen/GameOverScreen.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default function GameOverScreen (){
+    return (
+        <></>
+    )
+}
+const styles = StyleSheet.create({
+
+})
---
+++ screen/StartGameScreen.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default function StartGameScreen(){
+    return (
+        <></>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
