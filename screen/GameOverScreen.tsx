4f14a51c3b95492bb6930f3df090354e340f07fb
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-11-23 17:08:07 +0700

    Using & styling Nested Text

--- screen/GameOverScreen.tsx
+++ screen/GameOverScreen.tsx
@@ -7,29 +7,44 @@ export default function GameOverScreen() {
     <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
       <View style={styles.imageContainer}>
-        <Image style={styles.imange} source={require("../assets/images/success.png")} />
+        <Image
+          style={styles.imange}
+          source={require("../assets/images/success.png")}
+        />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.sumaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent:'center',
-    alignItems:'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
     width: 300,
     height: 300,
     borderRadius: 150,
-    borderWidth:3,
+    borderWidth: 3,
     borderColor: Colors.primary800,
-    overflow:'hidden',
-    margin:36
+    overflow: "hidden",
+    margin: 36,
   },
   imange: {
-    width:'100%',
-    height:'100%'
+    width: "100%",
+    height: "100%",
+  },
+  sumaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
